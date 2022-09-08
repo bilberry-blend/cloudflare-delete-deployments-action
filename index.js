f1dc448168130504868eeb8bfb9f3843289e0202
Snorre Magnus Davøen <snorre.magnus.davoen@adventuretech.no>
2022-09-08 14:31:45 +0200

    feat: Implement cloudflare deployments delete action
    Uses the GitHub JavaScript action as a template for a new action that
    can be used to delete Cloudflare Deployments. The new action works by
    first paging through all the deployments available through the
    Cloudflare API. Then it filters the list of deployments down to those
    initiated by GitHub PRs and a matching branch name. It then deletes all
    but the latest deployment, which at the moment cannot be deleted.
    
    The action accepts the Cloudflare project name, account id, and token as
    well as the branch name to match against as action input variables. It
    masks the token to avoid the token being logged.
    
    If the action encounters any errors it will bail out and log an error to
    the action console.
    
    The action is unit tested using Jest mocks to mock out the API calls to
    Cloudflare. A e2e test is provided, but is skipped during CI as we don't
    want to perform actual requests against Cloudflare.
    
    Prettier is used to format code during development.
    
    Signed-off-by: Snorre Magnus Davøen <snorre.magnus.davoen@adventuretech.no>

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-const core = require('@actions/core');
-const wait = require('./wait');
+// @ts-check
+const core = require('@actions/core')
+const { main } = require('./main')
 
+// Input parameters needed to identify the deployments to delete
+const project = core.getInput('project')
+const account = core.getInput('account')
+const branch = core.getInput('branch')
 
-// most @actions toolkit packages have async methods
-async function run() {
-  try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+// Sensitive input parameters to authenticate with the API
+const token = core.getInput('token')
+core.setSecret(token) // Ensure Cloudflare token does not leak into logs
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+try {
+  main(project, account, branch, token)
+} catch (error) {
+  core.setFailed(error.message)
 }
-
-run();
---
+++ jest.config.js
@@ -0,0 +1,7 @@
+/** @type {import('jest').Config} */
+const config = {
+  verbose: true,
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+}
+
+module.exports = config
